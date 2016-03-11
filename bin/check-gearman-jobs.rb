eba382d1c8d1865263abe4f319dd9cf2b87768b5
Aaron Brady <aaron@insom.me.uk>
2016-03-11 17:11:58 +0000

    First go at the checks

---
+++ bin/check-gearman-jobs.rb
@@ -0,0 +1,112 @@
+#! /usr/bin/env ruby
+#
+# check-gearman-jobs
+#
+# DESCRIPTION:
+#   #YELLOW
+#
+# OUTPUT:
+#   plain-text
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: gearman-ruby
+#   gem: sensu-plugin
+#
+# USAGE:
+#   #YELLOW
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2016 Aaron Brady <aaron@iweb.co.uk>
+#   Copyright S. Zachariah Sprackett <zac@sprackett.com>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+#
+
+require 'sensu-plugin/check/cli'
+require 'gearman/server'
+
+#
+# Check Gearman Workers
+#
+class CheckGearmanWorkers < Sensu::Plugin::Check::CLI
+  option :host,
+         short: '-H HOST',
+         default: 'localhost'
+  option :port,
+         short: '-p PORT',
+         default: '4730'
+  option :queue,
+         short: '-q QUEUE'
+  option :crit_high,
+         short: '-c CRIT_HIGH_THRESHOLD',
+         proc: proc(&:to_i),
+         default: false
+  option :warn_high,
+         short: '-w WARN_HIGH_THRESHOLD',
+         proc: proc(&:to_i),
+         default: false
+  option :crit_low,
+         short: '-C CRIT_LOW_THRESHOLD',
+         proc: proc(&:to_i),
+         default: 0
+  option :warn_low,
+         short: '-W WARN_LOW_THRESHOLD',
+         proc: proc(&:to_i),
+         default: 0
+
+  def run
+    begin
+      gearman = Gearman::Server.new(
+        "#{config[:host]}:#{config[:port]}"
+      )
+    rescue => e
+      critical "Failed to connect: (#{e})"
+    end
+
+    stats = {}
+    criticals = []
+    warnings = []
+    okays = []
+
+    if config[:queue]
+      stat = gearman.status[config[:queue]]
+      if stat.nil?
+        warning "Queue #{config[:queue]} not found"
+      else
+        stats = {config[:queue] => stat}
+      end
+    else
+      stats = gearman.status
+    end
+
+    stats.each do |queue_name, stat|
+      jobs = stat[:queue].to_i
+      if config[:crit_high] && jobs > config[:crit_high]
+        criticals << "#{queue_name}: High threshold is #{config[:crit_high]} jobs (#{jobs} active jobs)"
+      elsif config[:warn_high] && jobs > config[:warn_high]
+        warnings << "#{queue_name}: High threshold is #{config[:warn_high]} jobs (#{jobs} active jobs)"
+      elsif jobs < config[:crit_low]
+        warnings << "#{queue_name}: Low threshold is #{config[:crit_low]} jobs (#{jobs} active jobs)"
+      elsif jobs < config[:warn_low]
+        warnings << "#{queue_name}: Low threshold is #{config[:warn_low]} (#{jobs} active jobs)"
+      else
+        okays << "#{queue_name}: #{jobs} jobs found."
+      end
+    end
+
+    if criticals.length > 0
+      critical criticals.join(" ")
+    end
+    if warnings.length > 0
+      warning warnings.join(" ")
+    end
+    if okays.length > 0
+      ok okays.join(" ")
+    end
+  end
+end
